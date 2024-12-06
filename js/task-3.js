4104806222d111c3f86fbef573a251c483f1cf62
amurovnaa <amurovna09@gmail.com>
2024-12-06 22:39:26 +0200

    add all

---
+++ js/task-3.js
@@ -0,0 +1,23 @@
+// Функція checkForSpam(message) приймає рядок(параметр message), перевіряє його на вміст заборонених слів spam і sale,
+// і повертає результат перевірки.Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
+
+// Доповни код функції таким чином, що:
+
+// Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
+// Якщо в рядку відсутні заборонені слова, функція повертає буль false
+
+function checkForSpam(message) {
+  const messageLowerCase = message.toLowerCase();
+  const hasbanWord1 = messageLowerCase.includes('spam');
+  const hasbanWord2 = messageLowerCase.includes('sale');
+  const hasbanWords = hasbanWord1 || hasbanWord2;
+  return hasbanWords;
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
